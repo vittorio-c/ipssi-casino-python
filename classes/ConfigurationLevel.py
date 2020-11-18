50b6cbe15b5a6b71cb732d135b4ac11cdb133b80
Sébastien Grivel <s.grivel@ecole-ipssi.net>
2020-11-18 16:28:17 +0100

    B-Gestion des Gains

--- classes/ConfigurationLevel.py
+++ classes/ConfigurationLevel.py
@@ -1,13 +1,38 @@
+import math
+
 class ConfigurationLevel :
     """ Contient les informations d'un niveau """
 
     number = None
     nb_try = None
     interval = None
-    gain = []
+    gain = {}
 
     def __init__(self, number, nb_try, interval, gain = []) :
         self.number = number
         self.nb_try = nb_try
         self.interval = interval
-        self.gain = gain
+        #self.gain = gain
+
+    def generateArrayGain(self, mise) :
+        self.gain["1"] = {}
+        self.gain["2"] = {}
+        self.gain["3"] = {}
+
+        self.gain["1"]["1"] = math.ceil(mise * 2)
+        self.gain["1"]["2"] = math.ceil(mise)
+        self.gain["1"]["3"] = math.ceil(mise / 2)
+
+        self.gain["2"]["1"] = math.ceil(mise * 3)
+        self.gain["2"]["2"] = math.ceil(mise * 2)
+        self.gain["2"]["3"] = math.ceil(mise)
+        self.gain["2"]["4"] = math.ceil(mise / 2)
+        self.gain["2"]["5"] = math.ceil(mise / 3)
+
+        self.gain["3"]["1"] = math.ceil(mise * 4)
+        self.gain["3"]["2"] = math.ceil(mise * 3)
+        self.gain["3"]["3"] = math.ceil(mise * 2)
+        self.gain["3"]["4"] = math.ceil(mise)
+        self.gain["3"]["5"] = math.ceil(mise / 2)
+        self.gain["3"]["6"] = math.ceil(mise / 3)
+        self.gain["3"]["7"] = math.ceil(mise / 4)
